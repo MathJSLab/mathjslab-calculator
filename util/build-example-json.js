e7e77cae46565e4d5aa440dd6d3a3fb7096a0e89
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2023-09-09 21:57:17 -0300

    Initial Commit

--- util/build-example-json.js
+++ util/build-example-json.js
@@ -13,3 +13,21 @@ for (let name in example) {
 fs.writeFileSync(path.resolve(__dirname, '..', 'src', 'example.json'), JSON.stringify(example, null, 2));
 
 console.log('Building example.json done.')
+
+try {
+    let directory = path.resolve(__dirname, '..', 'dist');
+    if (fs.existsSync(directory)) {
+        fs.rmSync(directory, { recursive: true });
+        fs.mkdirSync(directory);
+    }
+    else {
+        fs.mkdirSync(directory);
+    }
+    directory = path.resolve(__dirname, '..', 'dist', 'example');
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory);
+    }
+    fs.copyFileSync(path.resolve(__dirname, '..', 'example', 'example.json'), path.resolve(__dirname, '..', 'dist', 'example', 'example.json'));
+  } catch (err) {
+    console.error(err);
+  }
