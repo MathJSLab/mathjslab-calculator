7fed210f95046fca9688129e3e6967672d8bc105
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2023-10-08 02:23:04 -0300

    Initial Commit

--- script/easter.m
+++ script/easter.m
@@ -1,3 +1,18 @@
-
-MONTH = 3
-DAY = 19
+% Name: easter.m
+% Purpose: Calculate Easter Sunday for a given year.
+% Source: Jean Meeus, Astronomical Algorithms ; Richmond (Virginia, États-Unis), Willmann-Bell, 1998, pp. 67–68.
+% preconditions: variable YEAR defined.
+n = mod(YEAR, 19)                                % Metonic cycle
+c = fix(YEAR / 100)                              % number of centuries completed
+u = mod(YEAR, 100)                               % past years beyond complete centuries
+s = fix(c / 4)                                   % number of completed leap centuries Gregorian cycles (400 year cycles, 97 leap years)
+t = mod(c, 4)                                    % centuries beyond complete leap cycles
+p = fix((c + 8) / 25)                            % proemptosis cycles completed
+q = fix((c - p + 1) / 3)                         % proemptosis passed beyond full cycles
+E = mod((19 * n + c - s - q + 15), 30)           % epacta
+b = fix(u / 4)                                   % number of leap years Julian cycles completed
+d = mod(u, 4)                                    % years beyond the Julian cycles of complete leap years
+L = mod((32 + 2 * t + 2 * b - E - d), 7)         % dominical letter
+h = fix((n + 11 * E + 22 * L) / 451)             % correction
+MONTH = fix((E + L - 7 * h + 114) / 31) - 1      % month
+DAY = mod(1 + (E + L - 7 * h + 114), 31)         % day
