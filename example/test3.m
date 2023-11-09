1c7bdd86353d3ef138167c48d6926fbc372a8c14
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2023-11-09 19:18:50 -0300

    Initial Commit

--- example/test3.m
+++ example/test3.m
@@ -1,10 +1,1 @@
 clear
-A = [1,2,3; 4,5,6; 7,8,9]
-A(2,3)
-A(2,end)
-A(3,2)
-A(3,end-1)
-B = [16,5,9,4,2,11,7,14]
-B([5:8, 1:4]) % Extract and swap the halves of B
-B(1:2:end)   % Extract all the odd elements
-B(end:-1:1)   % Reverse the order of elements
