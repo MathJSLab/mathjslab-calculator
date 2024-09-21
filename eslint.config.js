49c49cf4d08f0177058766fb5ac3a01f194641c4
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2024-09-21 18:52:25 -0300

    Initial Commit

--- eslint.config.js
+++ eslint.config.js
@@ -36,6 +36,7 @@ module.exports = [
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': 'warn',
             '@typescript-eslint/ban-types': 'off',
+            '@typescript-eslint/no-unsafe-function-type': 'off',
             'no-console': 'warn',
             ...prettierPlugin.configs.recommended.rules,
             ...eslintConfigPrettier.rules,
